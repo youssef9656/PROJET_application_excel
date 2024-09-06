91ec7ac66e047123ba6e0f8e82fb3166db8a87ce
YOUSEF <2004122200162@ofppt-edu.ma>
2024-09-06 17:31:48 +0100

    add.

--- Application/View/opration/option_Ent_Sor.php
+++ Application/View/opration/option_Ent_Sor.php
@@ -2,21 +2,32 @@
 
 <?php include '../../Config/connect_db.php'; $pageName= 'Catalogue du temps'; ?>
 <?php
-$querySelect = "SELECT * FROM fournisseurs";
+$querySelect = "SELECT * FROM lots";
 $paramsSelect = [];
 $data = selectData($querySelect, $paramsSelect);
 
-$queryProduit = "SELECT DISTINCT nom_fournisseur FROM fournisseurs";
-$nom_fournisseur = selectData($queryProduit, []);
+$queryProduit = "SELECT DISTINCT lot_name FROM lots";
+$lot_name = selectData($queryProduit, []);
 
 $queryElement = "SELECT DISTINCT prenom_fournisseur FROM fournisseurs";
 $prenom_fournisseur = selectData($queryElement, []);
 
-//$queryFamille = "SELECT DISTINCT FamilleOperation FROM fournisseurs";
-//$Familles = selectData($queryFamille, []);
+if( !empty(isset($_GET["lot_name"]))){
+    $lot_name = $_GET["lot_name"];
+    $query2 = "
+    SELECT sous_lots.sous_lot_id, sous_lots.sous_lot_name, sous_lots.lot_id, lots.lot_name
+    FROM sous_lots
+    JOIN lots ON sous_lots.lot_id = lots.lot_id where lots.lot_name ='$lot_name'";
+    $result2 = mysqli_query($conn, $query2);
+};
+
+if (!empty(isset($_GET["lot_name"])) && !empty(isset($_GET["lot_name"]))){
+
+
+
+
+}
 ?>
-
-
 
 <!doctype html>
 <html lang="en">
@@ -47,8 +58,8 @@ include '../../includes/header.php';
                                id="nom_fournisseur" onchange="filterTable()">
                         <datalist id='nom'>
                             <option value=""></option>
-                            <?php foreach($nom_fournisseur as $p):?>
-                                <option value='<?= $p['nom_fournisseur'] ?>'><?= $p['nom_fournisseur'] ?></option>
+                            <?php foreach($lot_name as $p):?>
+                                <option value='<?= $p['lot_name'] ?>'><?= $p['lot_name'] ?></option>
                             <?php endforeach;?>
                         </datalist>
                     </div>
@@ -80,5 +91,12 @@ include '../../includes/header.php';
 
 </body>
 <script src="../../includes/js/bootstrap.bundle.min.js"></script>
+<script>
+    $('#tbl_fournisseur').load('afficherfournisseur.php #tblfr',function (){
 
+
+    });
+
+
+</script>
 </html>
