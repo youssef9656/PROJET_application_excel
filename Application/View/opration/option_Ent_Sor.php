f3068dc3b1223e14fe88d40bf31c0bb6529c3bfa
YOUSEF <2004122200162@ofppt-edu.ma>
2024-09-06 17:03:26 +0100

    add.

--- Application/View/opration/option_Ent_Sor.php
+++ Application/View/opration/option_Ent_Sor.php
@@ -35,7 +35,41 @@ $prenom_fournisseur = selectData($queryElement, []);
 $pageName= 'Opration';
 include '../../includes/header.php';
 ?>
+<div>
+    <div style="display:flex;flex-flow: row wrap ; ">
+        <form>
+        <h4 class="mt-2 ms-5"> Opration  <button class="show-form-btn" onclick="toggleForm()">+</button> </h4>
+        <div class="col ms-5 mt-2">
+            <div class="filter-inputs mb-3">
+                <div class="form row" >
+                    <div class="col-3">
+                        <input type="text" list='nom' class="form-control keepDatalist" placeholder="Nom"
+                               id="nom_fournisseur" onchange="filterTable()">
+                        <datalist id='nom'>
+                            <option value=""></option>
+                            <?php foreach($nom_fournisseur as $p):?>
+                                <option value='<?= $p['nom_fournisseur'] ?>'><?= $p['nom_fournisseur'] ?></option>
+                            <?php endforeach;?>
+                        </datalist>
+                    </div>
+                    <div class="col-3">
+                        <input type="text" list='prenom' class="form-control keepDatalist" placeholder="Prenom"
+                               id="prenom_fournisseur" onchange="filterTable()">
+                        <datalist id='prenom'>
+                            <option value=""></option>
+                            <?php foreach($prenom_fournisseur as $e):?>
+                                <option value='<?= $e['prenom_fournisseur'] ?>'><?= $e['prenom_fournisseur'] ?></option>
+                            <?php endforeach;?>
+                        </datalist>
+                    </div>
+                </div>
+            </div>
+        </form>
 
+        </div>
+    </div>
+
+</div>
 
 
 
